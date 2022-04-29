31cd294607513c792e2962c7d363e53acee6cb8f
Chema Torres <chemaaa94@gmail.com>
2022-04-29 16:30:52 -0500

    using {} to dynamic expressions

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 const Expenseitem = () => {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = "Car Insurance";
+    const expenseAmout = 294.67;
+
   return (
-    <div>
-      <div>March 28th 2021</div>
-      <div>
-        <h2>Car Insurence</h2>
-        <div>294.67</div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmout}</div>
       </div>
     </div>
   );
