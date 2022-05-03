7d27612447a30c6ca5a538cec02297cbef6e877b
Chema Torres <chemaaa94@gmail.com>
2022-05-02 19:25:59 -0500

    learning addEventListeners in REACT

--- src/components/Expenses/Expenseitem.js
+++ src/components/Expenses/Expenseitem.js
@@ -5,6 +5,10 @@ import React from "react";
 
 
 const Expenseitem = (props) => {
+  const clickHandler =()=>{
+    console.log("Click")
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -12,6 +16,7 @@ const Expenseitem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
