b41372a5c0787fb0509fa81e6fba9b27d791932a
Chema Torres <chemaaa94@gmail.com>
2022-05-05 19:03:57 -0500

    Output dynamic list using map

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,39 +1,29 @@
 import Expenseitem from "./Expenseitem";
 import "./Expenses.css";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Card from "../IU/Card";
 import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
-
   const [filteredYear, setFilteredYear] = useState("2020");
 
-  const filterChangeHandler = selectedYear =>{
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-  }
+  };
   return (
     <Card className="expenses">
-      <ExpenseFilter selected={filteredYear} onChangeFilter ={filterChangeHandler} />
-      <Expenseitem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
       />
-      <Expenseitem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <Expenseitem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <Expenseitem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      {props.items.map((expense) => (
+        <Expenseitem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+
     </Card>
   );
 };
